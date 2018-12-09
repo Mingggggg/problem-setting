6e2a0362188070c2fe2f7f2b4cd77cc1a05cb2d2
Mingyang Wang <mw3165@nyu.edu>
2018-12-09 16:25:27 -0500

    housing keeping | tests

--- gen/gen.py
+++ gen/gen.py
@@ -1,17 +1,1 @@
-import sys
-from random import randint
-
-n = 10
-m = 10
-
-with open("test.in", "w") as out:
-    out.write("%d %d %d\n" % (n, m, 1000))
-    i = 0
-    for x in range(0, n):
-        for y in range(0, m):
-            out.write("%d " % (randint(1, 10)))
-        out.write("\n")
-    for x in range(0, n):
-        for y in range(0, m):
-            out.write("%d " % (randint(1, 10)))
-        out.write("\n")
+# We've already generated our random testcases
