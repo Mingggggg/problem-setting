bf0004318c3e269b2227cfccfdfdb710af2b11eb
chennyhaohao <chennyhaohao@gmail.com>
2018-12-09 16:25:43 -0500

    Update validator.py

--- input_format_validators/validator.py
+++ input_format_validators/validator.py
@@ -1,23 +1,51 @@
-# This is a python2 validator
-
 import sys, re
 
 n_line = sys.stdin.readline()
 
-if re.match('^[1-9]\d*$', n_line) == None:
-  sys.exit(1) # invalid n
+if re.match('^([1-9]\d*) ([1-9]\d*) ([1-9]\d*)$', n_line) == None:
+	sys.exit(1) # invalid n & m
 
-n = int(n_line)
+l = n_line.split(" ")
+n = int(l[0])
+m = int(l[1])
+k = int(l[2])
+if (not (n <= 100 and m <= 100 and k <= 1000)):
+	#print("1")
+	sys.exit(1) # invalid n & m
 
-nums_line = sys.stdin.readline()
-# Here we match n integers. An integer must not have leading zero.
-if re.match('^(0|-?[1-9]\d*)( (0|-?[1-9]\d*)){%d,%d}$' % (n-1, n-1), nums_line) == None:
-  sys.exit(2) # invalid numbers
+for i in range(n):
+	nums_line = sys.stdin.readline()
+# Here we match m integers. An integer must not have leading zero.
+	if re.match('^(0|[1-9]\d*)( (0|[1-9]\d*)){%d}$' % (m-1), nums_line) == None:
+		#print("2")
+		sys.exit(2) # invalid distances
 
-nums = [int(x) for x in nums_line.split(' ')]
+	nums = [int(x) for x in nums_line.split(' ')]
+	if i == 0:
+		if nums[0] != 0:
+			#print("3")
+			sys.exit(3) # distance out of range
+		nums = nums[1:]
+	if max(nums) > 1e9 or min(nums) < 1:
+		#print("3")
+		sys.exit(3) # distances out of range
 
-if max(nums) > 100 or min(nums) < -100:
-  sys.exit(3) # numbers out of range
+for i in range(n):
+	nums_line = sys.stdin.readline()
+# Here we match m integers. An integer must not have leading zero.
+	if re.match('^(0|[1-9]\d*)( (0|[1-9]\d*)){%d}$' % (m-1), nums_line) == None:
+		#print("4")
+		sys.exit(4) # invalid p
 
+	nums = [int(x) for x in nums_line.split(' ')]
+	if i == 0:
+		if nums[0] != 0:
+			#print("3")
+			sys.exit(3) # distance out of range
+		nums = nums[1:]
+	if max(nums) > 1000 or min(nums) < 1:
+		#print("5")
+		sys.exit(5) # p out of range
 # an input validator must exit with code 42 to for success
+#print("42")
 sys.exit(42)
